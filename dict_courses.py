0fad4d893827ad8c7124673b4f8671667d247602
HemantSrivastava01 <54182643+HemantSrivastava01@users.noreply.github.com>
2021-01-25 08:57:56 +0530

    Add files via upload

---
+++ dict_courses.py
@@ -0,0 +1,10 @@
+courses = {
+    1: 'python',
+    2: 'data science',
+    'third': 'ML'
+}
+print("entered Dictionary is :", courses)
+
+courses['third'] = 'hadoop'
+courses['fourth'] = 'machine learning'
+print("new dictionary is :", courses)
